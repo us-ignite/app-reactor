3c7b966b528a47466b49bda28ff5e7b484875931
Scott Turnbull <streamweaver@gmail.com>
2017-08-22 10:38:08 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { IdeasComponent } from './components/ideas/ideas.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EchosystemComponent } from './components/echosystem/echosystem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    IdeasComponent,
+    FooterComponent,
+    EchosystemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/echosystem/echosystem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-echosystem',
+  templateUrl: './echosystem.component.html',
+  styleUrls: ['./echosystem.component.scss']
+})
+export class EchosystemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
