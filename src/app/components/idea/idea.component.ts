52762c626aff8f5a55691ec21d9aa3d1616fe78d
Scott Turnbull <streamweaver@gmail.com>
2017-08-29 13:34:53 -0400

    Beta version test complete

--- src/app/components/idea/idea.component.ts
+++ src/app/components/idea/idea.component.ts
@@ -8,10 +8,12 @@ import { Scenario, Idea, Example } from '../../models/scenario';
 })
 export class IdeaComponent implements OnInit {
   @Input('scenario') scenario: Scenario;
+  @Input('odd') odd: boolean;
   solution: string;
   showIdeas: boolean;
   examples: Example[];
-  watchChange: boolean;
+  showExamples: boolean;
+  mailTo: string;
 
   constructor() { }
 
@@ -26,19 +28,26 @@ export class IdeaComponent implements OnInit {
   }
 
   resetExample() {
-    if (this.watchChange) {
+    if (this.showExamples) {
       this.examples = [];
     }
-    this.watchChange = false;
+    this.showExamples = false;
   }
 
   addSolution(solution: string, examples: Example[]) {
     this.solution = solution;
-    this.watchChange = true;
+    this.showExamples = false;
+    if (examples.length > 0) {
+      this.showExamples = true;
+    }
     this.examples = examples;
   }
 
-  mailTo() {
-    console.log(this.scenario.problem + this.solution + '.');
+  setMailTo() {
+    const linkText: string[] = ['mailto:?'];
+    linkText.push('subject=' + encodeURI('Gigabit App Idea from US Ignite'));
+    linkText.push('cc=' + encodeURI('scott.turnbull@us-ignite.org'));
+    linkText.push('body=' +  encodeURI('You created the following at the US Ignite Gig App Generator\n\n' + this.scenario.problem + ' ' + this.solution + '.'));
+    this.mailTo = linkText.join('&');
   }
 }
